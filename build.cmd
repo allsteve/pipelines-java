c909bdfea0f3bb3a1049527057599d6274117eb7
Steve Allison <allsteve@users.noreply.github.com>
2019-10-22 22:52:37 +1100

    create build\bin dir

--- build.cmd
+++ build.cmd
@@ -8,6 +8,8 @@ echo Build.ArtifactStagingDirectory $(Build.ArtifactStagingDirectory)
 
 if not exist "./\build\_CPack_Packages\win64\ZIP" mkdir "./\build\_CPack_Packages\win64\ZIP"
 
+if not exist "./\build\bin" mkdir "./\build\bin"
+
 echo.> ./\build\_CPack_Packages\win64\ZIP\thing.zip
 
 dir $(Build.ArtifactStagingDirectory)\_build
