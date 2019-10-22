bd89be093de7633ff3b0273ce5f8d1f0e1dfdf77
Steve Allison <allsteve@users.noreply.github.com>
2019-10-22 22:47:01 +1100

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,9 @@
 @echo
 
+echo %*
+
+dir /s
+
 echo Build.ArtifactStagingDirectory $(Build.ArtifactStagingDirectory)
 
 if not exist "./\build\_CPack_Packages\win64\ZIP" mkdir "./\build\_CPack_Packages\win64\ZIP"
@@ -7,3 +11,5 @@ if not exist "./\build\_CPack_Packages\win64\ZIP" mkdir "./\build\_CPack_Package
 echo.> ./\build\_CPack_Packages\win64\ZIP\thing.zip
 
 dir $(Build.ArtifactStagingDirectory)\_build
+
+dir /s
